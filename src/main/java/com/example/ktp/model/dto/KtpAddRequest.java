e266b74fc7529b7a75a73b869461247615882530
ZhilPro27 <zhilalkrisna@gmail.com>
2026-03-10 16:07:36 +0700

    Mendefiniskan class KtpAddRequest

--- src/main/java/com/example/ktp/model/dto/KtpAddRequest.java
+++ src/main/java/com/example/ktp/model/dto/KtpAddRequest.java
@@ -1,4 +1,14 @@
 package com.example.ktp.model.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class KtpAddRequest {
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
 }
