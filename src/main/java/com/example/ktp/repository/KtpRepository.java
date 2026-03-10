39a5541f63b6b196b7fa7ac88a2a6c77cec2886e
ZhilPro27 <zhilalkrisna@gmail.com>
2026-03-10 16:11:00 +0700

    Mendefinisikan interface KtpRepository

--- src/main/java/com/example/ktp/repository/KtpRepository.java
+++ src/main/java/com/example/ktp/repository/KtpRepository.java
@@ -1,4 +1,9 @@
 package com.example.ktp.repository;
 
-public class KtpRepository {
+import com.example.ktp.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, String> {
 }
