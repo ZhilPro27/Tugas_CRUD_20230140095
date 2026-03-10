294a01a63939f621152c572e0e5ad70675c31a8e
ZhilPro27 <zhilalkrisna@gmail.com>
2026-03-10 17:44:14 +0700

    Mendefinisikan interface KtpService

--- src/main/java/com/example/ktp/service/KtpService.java
+++ src/main/java/com/example/ktp/service/KtpService.java
@@ -1,4 +1,15 @@
 package com.example.ktp.service;
 
-public class KtpService {
+
+import com.example.ktp.model.dto.KtpAddRequest;
+import com.example.ktp.model.dto.KtpDto;
+
+import java.util.List;
+
+public interface KtpService {
+    KtpDto AddKtp(KtpAddRequest request);
+    List<KtpDto> getAllKtp();
+    KtpDto getKtpById(int id);
+    KtpDto updateKtp(int id, KtpAddRequest request);
+    void DeleteKtp(int id);
 }
