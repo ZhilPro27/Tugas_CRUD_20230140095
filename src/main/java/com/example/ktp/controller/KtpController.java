768853cacdeffd86ef267db8fab82a6781f81fce
ZhilPro27 <zhilalkrisna@gmail.com>
2026-03-10 18:10:53 +0700

    Mendefinisikan KtpController

--- src/main/java/com/example/ktp/controller/KtpController.java
+++ src/main/java/com/example/ktp/controller/KtpController.java
@@ -1,4 +1,80 @@
 package com.example.ktp.controller;
 
+import com.example.ktp.model.dto.KtpAddRequest;
+import com.example.ktp.model.dto.KtpDto;
+import com.example.ktp.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class KtpController {
+    @Autowired
+    private KtpService ktpService;
+
+    @PostMapping(
+            path = "/api/ktp",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> AddKtp(@RequestBody KtpAddRequest request){
+        KtpDto result = ktpService.AddKtp(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                "status", "success", "data", result
+        ));
+    }
+
+    @GetMapping(
+            path = "/api/ktp",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> getAllKtp() {
+        List<KtpDto> result = ktpService.getAllKtp();
+        return  ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                "status", "success", "data", result
+        ));
+    }
+
+    @GetMapping(
+            path = "/api/ktp/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> getAllKtById(@PathVariable("id") int id) {
+        KtpDto result = ktpService.getKtpById(id);
+        return  ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                "status", "success", "data", result
+        ));
+    }
+
+    @PutMapping(
+            path = "/api/ktp/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> UpdateKtp(
+            @PathVariable("id") int id,
+            @RequestBody KtpAddRequest request
+    ) {
+        KtpDto result = ktpService.updateKtp(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                "status", "success", "data", result
+        ));
+    }
+
+    @DeleteMapping(
+            path = "/api/ktp/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> DeleteKtp(@PathVariable("id") int id) {
+        ktpService.DeleteKtp(id);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                "status", "success deleted ktp with id " + id
+        ));
+    }
 }
