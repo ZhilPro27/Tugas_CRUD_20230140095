40b92070d689cd5831f9d558ebffba572a39f013
ZhilPro27 <zhilalkrisna@gmail.com>
2026-03-10 16:01:23 +0700

    Mendefinisikan entity class Ktp.java

--- src/main/java/com/example/ktp/model/entity/Ktp.java
+++ src/main/java/com/example/ktp/model/entity/Ktp.java
@@ -1,4 +1,37 @@
 package com.example.ktp.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "KTP")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "nomorKtp", nullable = false, unique = true)
+    private String nomorKtp;
+
+    @Column(name = "namaLengkap", nullable = false)
+    private String namaLengkap;
+
+    @Column(name = "alamat", nullable = false)
+    private String alamat;
+
+    @Column(name = "tanggalLahir", nullable = false)
+    private LocalDate tanggalLahir;
+
+    @Column(name = "jenisKelamin", nullable = false)
+    private String jenisKelamin;
 }
